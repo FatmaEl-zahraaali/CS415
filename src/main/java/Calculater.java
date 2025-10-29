9e9afd658954a1e5b72034900b0fdd4976402aa6
FatmaEl-zahraaali <fatmaali8053@gmail.com>
2025-10-29 21:17:51 +0300

    First commit

---
+++ src/main/java/Calculater.java
@@ -0,0 +1,6 @@
+public class Calculater {
+
+        public int add(int a, int b) {
+            return a + b;
+        }}
+
---
+++ src/test/java/Calculatertest.java
@@ -0,0 +1,12 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class Calculatertest {
+
+    @Test
+    void testAdd() {
+        Calculater calc = new Calculater();
+        assertEquals(5, calc.add(2, 3));
+    }}
+
